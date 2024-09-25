2a93bbfe71a7c7a174ac0d1824583b98c1deb8c9
FernandoGalvezLuis <fernandogalvezluis@hotmail.com>
2024-09-24 22:43:52 -0400

    refactored environment variables

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ import SearchBar from './components/SearchBar';
 import SearchResults from './components/SearchResults';
 import ExistingPlaylists from './components/ExistingPlaylists';
 import styles from './styles/App.module.css'
-import dotenv from 'dotenv';
-dotenv.config();
+
 
 const client_id = process.env.CLIENT_ID;
 const client_secret = process.env.CLIENT_SECRET;
